7806a55c7ef999815d0ec8a10fa3e4ace7fa8101
Elian Remaggi <elianremaggi@gmail.com>
2025-05-17 18:03:59 -0300

    FEATURE ( ScrollPositiion Component) : new component
    See all the time Scroll position Value

---
+++ components/ScrollPosition.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+
+import type { Ref } from 'vue';
+
+const injectedScrollValue = inject<Ref<number>>('scrollValue', ref(0));
+
+// Computed property que usa la prop scrollValue
+const printScrollValue = () => {
+    console.log('Scroll actual:', injectedScrollValue.value);
+};
+</script>
+
+<template>
+    <div class="fixed bottom-4 right-4 z-50 p-3 bg-black bg-opacity-70 text-white rounded-lg cursor-pointer"
+        @click="printScrollValue">
+        Scroll position: {{ injectedScrollValue }} px
+    </div>
+</template>
