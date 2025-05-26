430e935419ac424dc6c70a9055152ff4894f9d4e
Elian Remaggi <elianremaggi@gmail.com>
2025-05-25 21:26:26 -0300

    FEAT ( presentation ) Change styles, add underline, gradiants

--- components/presentation.vue
+++ components/presentation.vue
@@ -6,7 +6,7 @@ import { computed } from 'vue';
 const injectedScroll = inject<Ref<number>>('scrollValue', ref(0));
 
 const ANIMATION_START = 0;
-const ANIMATION_END = 1000;
+const ANIMATION_END = 2000;
 
 const shouldRender = computed(() => {
     return injectedScroll.value >= ANIMATION_START && injectedScroll.value <= ANIMATION_END;
@@ -17,24 +17,22 @@ const shouldRender = computed(() => {
 
 <template>
     <div :class="{ 'hidden': !shouldRender }"
-        class="relative w-full h-screen bg-gradient-to-b from-blue-800 to-indigo-950 overflow-hidden">
+        class="relative w-full h-screen  ">
         <particles/>
-        <!-- Contenido centrado responsivo -->
-        <div class="flex flex-col absolute inset-0  items-center justify-center p-4">
+
+        <div class="flex flex-col absolute inset-0  items-center mt-16 p-4">
             <div class="relative rounded-full w-48 h-48 md:w-80 md:h-80 mb-8">
-                <!-- Gradiente como pseudo-elemento -->
                 <div
                     class="absolute -inset-1 bg-gradient-to-r from-azul-400 via-azul-600 to-azul-800 rounded-full blur-sm">
                 </div>
-
-                <!-- Imagen con fondo blanco para el "borde" interior -->
-                <div class="absolute inset-0 bg-white rounded-full p-1">
+                <div class="absolute inset-0  rounded-full p-2 vue-gradient">
                     <img class=" w-full object-cover rounded-full" src="@/assets/foto.jpeg" alt="Foto de perfil">
                 </div>
             </div>
             <b id="name" class="text-4xl md:text-6xl font-bold text-white text-center">
-                <div>Elian</div>
-                <div>Remaggi</div>
+                <div>Elian Remaggi</div>
+                <div class="w-full vue-gradient h-1 rounded-full"></div>
+                <div class="mt-5">Software Developer</div>
             </b>
 
         </div>
