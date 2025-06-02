340f282823635037050cbb62456909cbd5f27e5d
Elian Remaggi <elianremaggi@gmail.com>
2025-06-02 19:52:19 -0300

    FIX ( Presentation component ) : Gsap console warning

--- components/ScrollStar.vue
+++ components/ScrollStar.vue
@@ -9,6 +9,7 @@ const resetScroll = () => {
 
 </script>
 <template>
+    <p>Holaaa</p>
     <div class="fixed bottom-10 right-10 z-50 p-3 bg-black bg-opacity-70 text-white rounded-lg cursor-pointer"> 
         <button @click="resetScroll"
             class="mt-8 px-8 py-3 rounded-full bg-gradient-to-r from-purple-600 to-cyan-500 text-white font-bold hover:from-purple-500 hover:to-cyan-400 transition-all duration-300 shadow-lg hover:shadow-cyan-500/30 flex items-center gap-2">
