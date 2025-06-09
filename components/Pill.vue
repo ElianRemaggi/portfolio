c402ad022730c373859e8df07167be16696fdfb0
Elian Remaggi <elianremaggi@gmail.com>
2025-06-09 20:45:15 -0300

    FEAT ( pill component ) : to tecnologies on experience secction

---
+++ components/Pill.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+const props = defineProps<{ text: string, primaryColor: string, secondaryColor: string }>();
+</script>
+
+<template>
+    <div class="rounded-full w-fit px-2 py-1" :class="[props.primaryColor, props.secondaryColor]">
+        <p>{{ props.text }}</p>
+    </div>
+</template>
