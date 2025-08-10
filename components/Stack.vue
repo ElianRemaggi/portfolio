b96ee77e5573c8cf4d6b775f16019207ebd276c1
Elian Remaggi <elianremaggi@gmail.com>
2025-08-10 19:00:56 -0300

    RELOAD

--- components/Stack.vue
+++ components/Stack.vue
@@ -4,7 +4,7 @@ import { computed, ref } from 'vue';
 const injectedScroll = inject<Ref<number>>('scrollValue', ref(0));
 
 const ANIMATION_START = 2800 ;  
-const ANIMATION_END = 3300 ;
+const ANIMATION_END = 3400 ;
 
 const shouldRender = computed(() => {
     return injectedScroll.value >= ANIMATION_START && injectedScroll.value <= ANIMATION_END;
